9ee4931119a660b02345130388b7b3b9b4e5f76d
Alejo <alejandrozamora68@gmail.com>
2022-10-01 19:16:11 -0500

    Arreglo de numeros de mayor a menor y pares

---
+++ Listas/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> car= new List<string>();
+
+car.Add("brand");
+car.Add("model");
+car.Add("color");
+
+for(int i = 0; i <car.Count; i++)
+{
+    Console.WriteLine(car[i]);
+}
+
+
